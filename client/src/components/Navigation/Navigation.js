6240b02e10929939d9816797e1f7f717b1241367
okputadora <okputadora@gmail.com>
2018-05-31 19:32:17 -0400

    heroku

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -7,8 +7,8 @@ const Navbar = (props) => {
   return (
 
     <div className={classes.NavBar}>
-    
-    <div className={classes.headerImage}> <img src={image} height="100px" /></div> <h1 className={classes.Title}> neighbors</h1> 
+
+      <div className={classes.headerImage}> <img src={image} height="100px" alt="profile"/></div> <h1 className={classes.Title}> neighbors</h1> 
         <div className={classes.Profile}>
           <Avatar context='navbar' username={props.username}/>
             </div>
