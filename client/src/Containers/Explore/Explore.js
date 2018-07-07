a9fe5cf50b8b86b8035540800b9fa3f98c427e03
Joseph Vega <1josephvega@gmail.com>
2018-07-07 12:46:27 -0400

    Added map to explore page, marker displays and moves onClick

--- client/src/Containers/Explore/Explore.js
+++ client/src/Containers/Explore/Explore.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import { compose, withStateHandlers } from "recompose";
+import { InfoWindow, withGoogleMap, withScriptjs, GoogleMap, Marker } from 'react-google-maps';
 
-class Explore extends Component {
+const Map = compose(
+    withStateHandlers(() => ({
+        isMarkerShown: false,
+        markerPosition: null
+      }), {
+        onMapClick: ({ isMarkerShown }) => (e) => ({
+            markerPosition: e.latLng,
+            isMarkerShown:true
+        })
+      }),
+    withScriptjs,
+    withGoogleMap
+)
+    (props =>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            onClick={props.onMapClick}
+        >
+            {props.isMarkerShown && <Marker position={props.markerPosition} />}
+
+        </GoogleMap>
+    )
+
+export default class Explore extends Component {
+    constructor(props) {
+        super(props)
+    }
 
     render() {
         return (
             <div className="container">
-                <h1> Explore Page! </h1>
+                <Map
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXYH0-ocvoZnzu1HrgZaBJQ1apvBclUt0"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
             </div>
         );
     }
   }
-
-
-  export default Explore;
