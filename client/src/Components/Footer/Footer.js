7fc49455d167c2fdccfb7ae443ead4862ee37648
okputadora <okputadora@gmail.com>
2018-07-07 11:28:58 -0400

    added message styling

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './Footer.css';
-import classes from './Footer.css';
-import StickyFooter from 'react-sticky-footer';
+// import classes from './Footer.css';
+// import StickyFooter from 'react-sticky-footer';
 
 const Footer = () => {
     return (
-        
+
         <div className="footer">
-            
+
         </div>
- 
- 
+
+
 
 )
 }
 
 export default Footer;
-
