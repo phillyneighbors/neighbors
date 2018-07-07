09219e20e968d6df3a1d62674446ff9903569064
okputadora <okputadora@gmail.com>
2018-07-07 11:37:23 -0400

    changed anchor tags to Links

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,68 +1,68 @@
 import React from 'react';
 import './Navigation.css';
 import classes from './Navigation.css';
-import image from './logo1.png'
-import Avatar from '../Avatar/Avatar'
+import image from './logo1.png';
+import { Link } from 'react-router-dom';
 
   const Navbar = () => {
   return (
     <div className={classes.Navbar}>
-    <nav className="navbar navbar-expand-lg navbar-light">
-    {/* <div className="animated bounce"> */}
-    <img src={image} height="100px" href="/" />
-    {/* </div> */}
-    <a className="navbar-brand mx-auto d-block text-center order-0" href="/">
-    {/* <div className="animated zoomInUp"> */}
-      <div className={classes.Title}>neighbors</div>
-    {/* </div> */}
-    </a>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        {/* <div className="animated bounce"> */}
+        <img src={image} height="100px" alt='logo'/>
+        {/* </div> */}
+        <Link className="navbar-brand mx-auto d-block text-center order-0" to="/">
+          {/* <div className="animated zoomInUp"> */}
+          <div className={classes.Title}>neighbors</div>
+          {/* </div> */}
+        </Link>
 
-    {/* <a href="/" class="navbar-brand mx-auto d-block text-center order-0 order-md-1 w-25">Brand</a>       */}
-  
+        {/* <a href="/" class="navbar-brand mx-auto d-block text-center order-0 order-md-1 w-25">Brand</a>       */}
 
-  
 
-    <button
-      className="navbar-toggler"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
 
- 
-    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-      <ul className="navbar-nav">
-        
-        
-        {/* Nav Link 1 */}
-        <li className="nav-item active">
-          <a className="nav-link" href="/chatRoom">
-            <span className={classes.NavLink}> chat</span> 
-          </a>
-        </li>
-      
 
-        {/* Nav Link 2 */}
-        <li className="nav-item active">
-          <a className="nav-link" href="/explore">
-            <span className={classes.NavLink}> explore</span> 
-          </a>
-        </li>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+        aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
 
-        {/* Nav Link 3 */}
-        <li className="nav-item active">
-          <a className="nav-link" href="/about">
-            <span className={classes.NavLink}>about </span> 
-          </a>
-        </li>
 
-      </ul>
-</div>
-</nav>
-</div>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+
+
+            {/* Nav Link 1 */}
+            <li className="nav-item active">
+              <Link className="nav-link" to="/chatRoom">
+                <span className={classes.NavLink}> chat</span>
+              </Link>
+            </li>
+
+
+            {/* Nav Link 2 */}
+            <li className="nav-item active">
+              <Link className="nav-link" to="/explore">
+                <span className={classes.NavLink}> explore</span>
+              </Link>
+            </li>
+
+            {/* Nav Link 3 */}
+            <li className="nav-item active">
+              <Link className="nav-link" to="/about">
+                <span className={classes.NavLink}>about </span>
+              </Link>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </div>
 
   )
 }
